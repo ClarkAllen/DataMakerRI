a7ab7f79da0a5cf2f0f3a856dda26b81c0451c18
Clark Allen <kitico@gmail.com>
2020-06-15 14:51:53 -0400

    Added log4j logging (to console only)

--- src/main/java/net/kbg/datamakerri/controllers/AddressController.java
+++ src/main/java/net/kbg/datamakerri/controllers/AddressController.java
@@ -21,6 +21,8 @@ import net.kb.datamaker.alpha.Gender;
 import net.kbg.datamakerri.helpers.AlphArgHelper;
 import net.kbg.datamakerri.model.Address;
 import net.kbg.datamakerri.model.ErrorMsg;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -35,14 +37,18 @@ import java.util.Optional;
 @RequestMapping("/v1/alph")
 public class AddressController {
 
+    private static final Logger log = LoggerFactory.getLogger(AddressController.class);
+
     @Autowired
     private AlphArgHelper argHelper;
 
     @GetMapping("/address")
     public ResponseEntity makeAddress(@RequestParam String gender, @RequestParam String namefmt) {
+        log.info("address to person : gender=" + gender + ", namefmt=" + namefmt);
         Optional<Gender> opGen = argHelper.genderArg(gender);
         Optional<Integer> opInt = argHelper.nameFormatArg(namefmt);
         if (opGen.isEmpty() || opInt.isEmpty()) {
+            log.error("400 Bad arguments");
             ErrorMsg errorMsg = new ErrorMsg("400", "Bad gender or name format argument");
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
