5c7c1d0f6eeaad51463891c2880fede15e4fa578
Clark Allen <kitico@gmail.com>
2020-07-01 15:45:56 -0400

    Added a boolean controller with one endpoint to start

---
+++ src/main/java/net/kbg/datamakerri/model/BooleanValue.java
@@ -0,0 +1,65 @@
+/*
+ *     Copyright 2020 Clark Allen
+ *
+ *     Licensed under the Apache License, Version 2.0 (the "License");
+ *     you may not use this file except in compliance with the License.
+ *     You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS IS" BASIS,
+ *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *     See the License for the specific language governing permissions and
+ *     limitations under the License.
+ */
+
+package net.kbg.datamakerri.model;
+
+public class BooleanValue {
+    private int intValue;
+    private boolean booleanValue;
+    private String stringValue;
+
+    public BooleanValue() {
+    }
+
+    public BooleanValue(int intValue, boolean booleanValue, String stringValue) {
+        this.intValue = intValue;
+        this.booleanValue = booleanValue;
+        this.stringValue = stringValue;
+    }
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public boolean isBooleanValue() {
+        return booleanValue;
+    }
+
+    public void setBooleanValue(boolean booleanValue) {
+        this.booleanValue = booleanValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    @Override
+    public String toString() {
+        return "BooleanValue{" +
+                "intValue=" + intValue +
+                ", booleanValue=" + booleanValue +
+                ", stringValue='" + stringValue + '\'' +
+                '}';
+    }
+}
