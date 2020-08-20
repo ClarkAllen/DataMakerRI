3311ce34e7d3a26f10322154767b6d0ba1bfe864
Clark Allen <kitico@gmail.com>
2020-08-19 20:54:59 -0400

    correcting bad annotation

--- src/main/java/net/kbg/datamakerri/controllers/alpha/NameController.java
+++ src/main/java/net/kbg/datamakerri/controllers/alpha/NameController.java
@@ -23,6 +23,7 @@ import net.kbg.datamakerri.services.alpha.NameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class NameController {
     @Autowired
     private NameService nameService;
 
-    @RequestMapping("/person")
+    @GetMapping("/person")
     public ResponseEntity makeNameOfPerson(@RequestParam String gender, @RequestParam String namefmt) {
 
         Optional<PersonName> optName = nameService.makeNameOfPerson(gender, namefmt);
